21a140164c5b573f08fb8084d7887182d39bc7e2
1273055646 <155231140+1273055646@users.noreply.github.com>
2024-09-16 16:19:25 +0800

    Update admin.py
    修改部署的错误代码

--- config/admin.py
+++ config/admin.py
@@ -10,8 +10,16 @@ class LinkAdmin(admin.ModelAdmin):
     list_display = ('title', 'href', 'status', 'weight', 'created_time')
     fields = ('title', 'href', 'status', 'weight')
 
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user  # 当前已经登录的用户作为作者
+        return super().save_model(request, obj, form, change)
+
 
 @admin.register(SideBar, site=custom_site)
 class SideBarAdmin(admin.ModelAdmin):
     list_display = ('title', 'display_type', 'content', 'created_time')
     fields = ('title', 'display_type', 'content')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super().save_model(request, obj, form, change)
